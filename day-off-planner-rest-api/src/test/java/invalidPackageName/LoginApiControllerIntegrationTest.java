8a7aba688c6e3ec6e6600c198d004569879e0465
Burhak <altair213@gmail.com>
2019-10-08 10:37:53 +0000

    Generated code by SwaggerHub

---
+++ day-off-planner-rest-api/src/test/java/invalidPackageName/LoginApiControllerIntegrationTest.java
@@ -0,0 +1,31 @@
+package invalidPackageName;
+
+import invalidPackageName.UserLoginApiModel;
+
+import java.util.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoginApiControllerIntegrationTest {
+
+    @Autowired
+    private LoginApi api;
+
+    @Test
+    public void loginUserTest() throws Exception {
+        UserLoginApiModel body = new UserLoginApiModel();
+        ResponseEntity<Void> responseEntity = api.loginUser(body);
+        assertEquals(HttpStatus.NOT_IMPLEMENTED, responseEntity.getStatusCode());
+    }
+
+}

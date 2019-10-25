70a49c0491ddc4ff71f62be0b99ec4f09cfd396a
Burhak <altair213@gmail.com>
2019-10-25 08:08:47 +0000

    Generated code by SwaggerHub

--- day-off-planner-rest-api/src/main/java/com/evolveum/day_off_planner_rest_api/model/UserApiModel.java
+++ day-off-planner-rest-api/src/main/java/com/evolveum/day_off_planner_rest_api/model/UserApiModel.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.*;
  * UserApiModel
  */
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-10-22T13:24:41.453Z[GMT]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-10-25T08:08:46.968Z[GMT]")
 public class UserApiModel   {
   @JsonProperty("id")
   private UUID id = null;

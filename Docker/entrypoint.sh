969845f22bae91cd37c8ac763ea480811112163a
Lukáš Havrišák <lukas.havrisak@student.tuke.sk>
2020-01-21 17:22:36 +0100

    update docker entrypoint to accept db user and password

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -6,6 +6,16 @@ if [[ -n "$1" ]]; then
   HOST="$1"
 fi
 
+DB_USER=""
+if [[ -n "$2" ]]; then
+  DB_USER="-Dspring.datasource.username=$2"
+fi
+
+DB_PASS=""
+if [[ -n "$3" ]]; then
+  DB_PASS="-Dspring.datasource.password=$3"
+fi
+
 echo "Running with host: $HOST"
 
-java -jar -Dhost="$HOST" /app/day-off-planner-rest.jar
+java -jar -Dhost="$HOST" "$DB_USER" "$DB_PASS" /app/day-off-planner-rest.jar
